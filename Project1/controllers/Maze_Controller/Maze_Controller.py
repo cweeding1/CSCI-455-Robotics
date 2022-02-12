3df4ee127dbb67782ecf150c84a8ab612997c998
Colton Weeding <49161177+cweeding1@users.noreply.github.com>
2022-02-12 16:52:14 -0700

    Updated Maze Controller.py

--- Project1/controllers/Maze_Controller/Maze_Controller.py
+++ Project1/controllers/Maze_Controller/Maze_Controller.py
@@ -1,195 +1,153 @@
 from controller import Robot, Motor, Device, TouchSensor, DistanceSensor, Gyro, Accelerometer
 import math
 
-TIME_STEP = 64
-MAX_SPEED = 6.28
+class RobotController:
 
-# create the Robot instance.
+    #constructor
+    def __init__(self, robot):
+        self.robot = robot
+        self.leftMotor = robot.getDevice('left wheel motor')
+        self.rightMotor = robot.getDevice('right wheel motor')
+        self.touchSensor = robot.getDevice("touch sensor")
+        self.compass = robot.getDevice("compass")
+        self.leftE = robot.getDevice("left wheel sensor")
+        self.rightE = robot.getDevice("right wheel sensor")
+        self.TIME_STEP = 64
+        self.MAX_SPEED = 6.28
+        self.psNames = ['ps0', 'ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'ps6', 'ps7']
+
+    #enable all of the robot devices used
+    def enable(self):
+        self.leftMotor = robot.getDevice('left wheel motor')
+        self.rightMotor = robot.getDevice('right wheel motor')
+        self.touchSensor = robot.getDevice("touch sensor")
+        self.touchSensor.enable(self.TIME_STEP)
+        self.compass = robot.getDevice("compass")
+        self.compass.enable(self.TIME_STEP)
+        self.leftE = robot.getDevice("left wheel sensor")
+        self.leftE.enable(self.TIME_STEP)
+        self.rightE = robot.getDevice("right wheel sensor")
+        self.rightE.enable(self.TIME_STEP)
+        for i in range(len(self.psNames)):
+            self.psNames[i] = self.robot.getDevice("ps" + str(i))
+            self.psNames[i].enable(self.TIME_STEP)
+        self.leftMotor.setPosition(float('inf'))
+        self.rightMotor.setPosition(float('inf'))
+
+    #set the velocity using a speed for the left wheel and right wheel
+    def velocity(self, leftSpeed, rightSpeed):
+        self.leftMotor.setVelocity(leftSpeed)
+        self.rightMotor.setVelocity(rightSpeed)
+
+    #detect if the touchsensor has hit the trophy
+    def isAtEnd(self):
+        if self.touchSensor.getValue() > 0:
+            return True
+        else:
+            return False
+
+    #detect if there is a wall to the front right
+    def frontWallRight(self):
+        if self.psNames[0].getValue() > 100:
+            return True
+        else:
+            return False
+
+    #detect if there is a wall to the front left
+    def frontWallLeft(self):
+        if self.psNames[7].getValue() > 100:
+            return True
+        else:
+            return False
+
+    #detect if there is a wall to the right
+    def wallRight(self):
+        if self.psNames[2].getValue() > 350:
+            return True
+        else:
+            return False
+
+    #detect if there is a wall to the left
+    def wallLeft(self):
+        if self.psNames[5].getValue() > 350:
+            return True
+        else:
+            return False
+
+    #detect if there is a corner to the right
+    def rightCorner(self):
+        if self.psNames[1].getValue() > 80:
+            return True
+        else:
+            return False
+
+    #detect if there is a corner to the left
+    def leftCorner(self):
+        if self.psNames[6].getValue() > 80:
+            return True
+        else:
+            return False
+
+    #right hand rule maze following algorithm
+    def rightHand(self):
+        self.enable()
+        while self.robot.step(self.TIME_STEP) != -1:
+            if self.isAtEnd():
+                print("Win")
+                self.velocity(0, 0)
+                break
+
+            else:
+                if self.frontWallRight():
+                    print("turn left")
+                    self.velocity(-self.MAX_SPEED, self.MAX_SPEED)
+
+                else:
+                    if self.wallRight():
+                        print("go forward")
+                        self.velocity(self.MAX_SPEED, self.MAX_SPEED)
+
+                    else:
+                        print("turn right")
+                        self.velocity(self.MAX_SPEED, self.MAX_SPEED/8)
+
+                    if self.rightCorner():
+                        print("turn left")
+                        self.velocity(self.MAX_SPEED/8, self.MAX_SPEED)
+
+    #left hand rule maze following algorithm
+    def leftHand(self):
+        self.enable()
+        while self.robot.step(self.TIME_STEP) != -1:
+            if self.isAtEnd():
+                print("Win")
+                self.velocity(0, 0)
+                break
+
+            else:
+                if self.frontWallLeft():
+                    print("turn right")
+                    self.velocity(self.MAX_SPEED, -self.MAX_SPEED)
+
+                else:
+                    if self.wallLeft():
+                        print("drive forward")
+                        self.velocity(self.MAX_SPEED, self.MAX_SPEED)
+
+                    else:
+                        print("turn left")
+                        self.velocity(self.MAX_SPEED/8, self.MAX_SPEED)
+
+                    if self.leftCorner():
+                        print("turn right")
+                        self.velocity(self.MAX_SPEED, self.MAX_SPEED/8)
+
+#create a robot
 robot = Robot()
 print("Starting")
 
-# get the motor devices
-leftMotor = robot.getDevice('left wheel motor')
-rightMotor = robot.getDevice('right wheel motor')
-# set the target position of the motors
-#rightMotor.setVelocity(0.5)
-#leftMotor.setVelocity(0.5)
+#make a new instance of robot
+newRobot = RobotController(robot)
 
-touchSensor = robot.getDevice("touch sensor")
-touchSensor.enable(TIME_STEP)
-compass = robot.getDevice("compass")
-compass.enable(TIME_STEP)
-leftE = robot.getDevice("left wheel sensor")
-leftE.enable(TIME_STEP)
-rightE = robot.getDevice("right wheel sensor")
-rightE.enable(TIME_STEP)
-gyro = robot.getDevice("gyro")
-gyro.enable(TIME_STEP)
-
-ps = []
-psNames = [
-    'ps0', 'ps1', 'ps2', 'ps3',
-    'ps4', 'ps5', 'ps6', 'ps7'
-]
-
-for i in range(8):
-    ps.append(robot.getDevice(psNames[i]))
-    ps[i].enable(TIME_STEP)
-
-leftMotor.setPosition(float('inf'))
-rightMotor.setPosition(float('inf'))
-leftMotor.setVelocity(2)
-rightMotor.setVelocity(2)
-
-
-#while robot.step(TIME_STEP) != -1:
-    #leftMotor.setPosition(float('inf'))
-    #rightMotor.setPosition(float('inf'))
-    #rightMotor.setVelocity(0.1)
-    #leftMotor.setVelocity(0.1)
-#    print(ps[4].getValue())
-#    if ps[4].getValue() > 100:
-#        print("wall")
-
-
-#are you at the end?
-   #if not is sensor 1 or 6 active
-        #if yes then rotate 180 degrees and restart
-    #if not is senor 0 or 1 active
-        #if yes then rotate left 90 degrees and restart
-    #if not is sensor 7 or 6 active
-        #if yes then rotate right 90 degrees and restart
-    #if not move forward and restart
-
-def isAtEnd():
-    return False
-    """
-    if touchSensor.getValue() > 0:
-        return True
-    else:
-        return False
-    """
-
-def rotate180():
-    print("Rotating 180 degrees")
-    leftMotor.setVelocity(-2)
-    rightMotor.setVelocity(2)
-    robot.step(2210)
-    leftMotor.setVelocity(0)
-    rightMotor.setVelocity(0)
-    return
-
-def rotateLeft():
-    print("Turning left")
-    leftMotor.setVelocity(-2)
-    rightMotor.setVelocity(2)
-    robot.step(1105)
-    leftMotor.setVelocity(0)
-    rightMotor.setVelocity(0)
-    return
-
-def rotateRight():
-    print("Turning right")
-    leftMotor.setVelocity(2)
-    rightMotor.setVelocity(-2)
-    robot.step(1105)
-    leftMotor.setVelocity(0)
-    rightMotor.setVelocity(0)
-    return
-
-def isWallLeft():
-    if ps[5].getValue() > 200:
-        return True
-    else:
-        return False
-
-def isWallRight():
-    if ps[2].getValue() > 200:
-        return True
-    else:
-        return False
-
-def isWallFront():
-    if ps[0].getValue() > 200 and ps[7].getValue() > 200:
-        return True
-    else:
-        return False
-
-def moveForward():
-    leftMotor.setVelocity(2)
-    rightMotor.setVelocity(2)
-
-
-
-while robot.step(TIME_STEP) != -1:
-    #print(ps[1].getValue())
-    """
-    if isAtEnd():
-        print("Finished")
-        break
-    elif ps[7].getValue() > 80 and ps[0].getValue() > 80:
-        leftMotor.setVelocity(0)
-        rightMotor.setVelocity(0)
-        rotate180()
-    elif ps[0].getValue() > 80 and ps[1].getValue() > 80:
-        leftMotor.setVelocity(0)
-        rightMotor.setVelocity(0)
-        rotateLeft()
-    elif ps[7].getValue() > 80 and ps[6].getValue() > 80:
-        leftMotor.setVelocity(0)
-        rightMotor.setVelocity(0)
-        rotateRight()
-    """
-
-    while isAtEnd() != True:
-        if isWallLeft():
-            if isWallFront():
-                moveForward()
-            else:
-                rotateRight()
-        else:
-            print("here")
-            #rotateLeft()
-    print("Finished")
-    break
-"""
-while robot.step(TIME_STEP) != -1:
-
-    rightMotor.setVelocity(-10)
-    leftMotor.setVelocity(10)
-
-    answer = compass.getValues()
-
-    if not math.isnan(answer[0]):
-        # print(answer)
-        #print("Touch Sensor: " + str(touchSensor.getValue()))
-
-        angle = (math.atan2(answer[0], answer[1]))
-        # print(angle)
-        if 0.77 > angle > -0.82:
-            print("East")
-        elif -0.82 > angle > -2.4:
-            print("South")
-        elif angle < -2.41 or angle > 2.44:
-            print("West")
-        else:
-            print("North")
-
-    else:
-        print("Not")
-
-    print(round(leftE.getValue()), 2)
-    print(round(rightE.getValue()), 2)
-
-"""
-
-
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-pass
+#newRobot.leftHand()
+newRobot.rightHand()
